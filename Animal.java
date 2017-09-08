93c7d1f3a12fbde6aee43ad662891016cfc5f9f9
adityashukla2799 <adityashukla2799@gmail.com>
2017-09-08 15:12:57 -0300

    Final

--- Animal.java
+++ Animal.java
@@ -13,9 +13,15 @@ public class Animal {
     private double speed;
     private float height;
     protected static int numberOfAnimals = 0;
-    static Scannner userinput = new Scanner(System.in)
+    static Scannner userinput = new Scanner(System.in);
 
     public Animal() {
-        System.out.println("")
+        numberOfAnimals++;
+        int sumOfNumbers = 5 + 1;
+        System.out.println("5 + 1 = " +sumOfNmubers);
     }
+    public static void main(String[] args){
+        Animal theAnimal = new Animal();
+    }
+
 }
