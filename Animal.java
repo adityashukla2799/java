d491cec3e9b7ad1b60b1508c75936a221da79de1
adityashukla2799 <adityashukla2799@gmail.com>
2017-09-08 14:05:47 -0300

    3 new java files added

---
+++ Animal.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+import java.util.*;
+
+public class Animal {
+    public static final double FAVNUMBER = 1.6180;
+    //final classes cannot be subclassed.
+    private String name;
+    private int weight;
+    private boolean hasOwner = false;
+    private byte age;
+
+
+}
